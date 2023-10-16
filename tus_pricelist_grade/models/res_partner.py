daac5fc1b0ae08cd5fbce233f3e706dea3d3e5bd
phutungthanhlong <phutungthanhlong@gmail.com>
2023-10-16 16:18:17 +0700

    thay

--- tus_pricelist_grade/models/res_partner.py
+++ tus_pricelist_grade/models/res_partner.py
@@ -4,20 +4,18 @@ class ResPartnerExt(models.Model):
     _inherit = 'res.partner'
 
     grade_id = fields.Many2one('grade.product.pricelist', string="Grade")
-    property_product_pricelist = fields.Many2one('product.pricelist', string="Pricelist")
+    property_product_pricelist = fields.Many2one('product.pricelist', string="Pricelist", compute='_compute_property_product_pricelist', store=True)
 
-    @api.onchange('grade_id')
-    def onchange_grade_id(self):
-        """
-        Update property_product_pricelist based on grade_id.
-        """
-        if self.grade_id:
-            # Tìm danh sách giá dựa trên grade_id
-            pricelist_ids = self.grade_id.pricelist_ids
-            if pricelist_ids:
-                # Lấy giá trị của pricelist từ danh sách giá
-                new_pricelist_id = pricelist_ids[0].id
-                self.property_product_pricelist = new_pricelist_id
-        else:
-            # Nếu grade_id bị xóa, bạn có thể đặt property_product_pricelist về giá trị mặc định hoặc giá trị rỗng tùy vào yêu cầu của bạn.
-            self.property_product_pricelist = False
+    @api.depends('grade_id')
+    def _compute_property_product_pricelist(self):
+        for partner in self:
+            if partner.grade_id:
+                new_pricelist_id = self._get_new_pricelist_id(partner.grade_id)
+                partner.property_product_pricelist = new_pricelist_id
+            else:
+                partner.property_product_pricelist = False
+
+    def _get_new_pricelist_id(self, grade):
+        # Lấy giá trị đầu tiên tương ứng với grade_id
+        new_pricelist_id = grade.pricelist_ids and grade.pricelist_ids[0].id
+        return new_pricelist_id
